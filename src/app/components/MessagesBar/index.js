4d2e00cf9ace4219b516c31f193f95419db66b4c
facuGarcia <facundo.garcia@widergy.com>
2021-09-20 09:45:33 -0300

    feat: message bar show and hide

--- src/app/components/MessagesBar/index.js
+++ src/app/components/MessagesBar/index.js
@@ -1,22 +1,29 @@
 /* eslint-disable react/prop-types */
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UTLabel } from '@widergy/energy-ui';
 import { connect } from 'react-redux';
 
 import styles from './styles.module.scss';
 
-class MessagesBar extends PureComponent {
-  render() {
-    return (
-      <div className={styles.container}>
-        <UTLabel bold large white>
-          {this.props.message}
-        </UTLabel>
-      </div>
-    );
-  }
-}
-// <img alt="logo" src={logo} className={styles.logo} />
+const MessagesBar = ({ message }) => {
+  const [showMessage, setshowMessage] = useState(false);
+
+  useEffect(() => {
+    setshowMessage(true);
+  }, [message]);
+
+  useEffect(() => {
+    if (showMessage) setTimeout(() => setshowMessage(false), 4000);
+  }, [showMessage]);
+
+  return (
+    <div className={showMessage ? styles.showMessage : styles.hiddenMessage}>
+      <UTLabel bold large white>
+        {message}
+      </UTLabel>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({ message: state.operationsRecord.messagesRecord.message });
 
